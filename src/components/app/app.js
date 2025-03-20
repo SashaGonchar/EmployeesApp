52e3d9632bb4caed945fecdae10435014cddbbc4
SashaGonchar <nekospoon228@gmail.com>
2025-03-20 23:51:10 +0200

    delete list item using trash icon added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import {Component} from 'react'
+
 import './app.css'
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
@@ -5,27 +7,48 @@ import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-function App () {
+class App extends Component {
 
-    const data = [
-        {name: "John Wick", salary:8000, increase: false, id: 1 },
-        {name: "Joe Biden", salary:10000, increase: true, id: 2 },
-        {name: "Nick Pick", salary:7000, increase: false, id: 3 },
-        {name: "Dark Rick", salary:3000, increase: false, id: 4 },
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: "John Wick", salary:8000, increase: false, id: 1 },
+                {name: "Joe Biden", salary:10000, increase: true, id: 2 },
+                {name: "Nick Pick", salary:7000, increase: false, id: 3 },
+                {name: "Dark Rick", salary:3000, increase: false, id: 4 },
+            ]
+        }
+    }
 
-    ]
-    return (
-        <div className={"app"}>
-            <AppInfo/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+            return {
+            data: data.filter(item => item.id !== id)
+            }
+
+        })
+    }
+
+
+    render() {
+        return (
+            <div className={"app"}>
+                <AppInfo/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,17 +1,42 @@
+import {Component} from 'react'
+
 import './employees-add-form.css'
 
-const EmployeesAddForm = () => {
-    return (
+class EmployeesAddForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+        [e.target.name]: e.target.value
+        })
+    }
+
+
+    render() {
+
+        const {name, salary} = this.state;
+
+        return (
         <div className={"app-add-form"}>
             <h3>Добавьте нового сотрудника</h3>
             <form className={"add-form d-flex"}>
-                <input type={"text"} className={"form-control new-post-label"} placeholder={"Как его зовут?"}/>
-                <input type={"number"} className={"form-control new-post-label"} placeholder={"З/П в $?"} />
+                <input name="name" onChange={this.onValueChange} value={name} type={"text"} className={"form-control new-post-label"} placeholder={"Как его зовут?"}/>
+                <input name="salary" onChange={this.onValueChange} value={salary} type={"number"} className={"form-control new-post-label"} placeholder={"З/П в $?"} />
 
                 <button type={"submit"} className={"btn btn-outline-light"}>Добавить</button>
             </form>
         </div>
     )
+    }
+
+
 }
 
 export default EmployeesAddForm;
