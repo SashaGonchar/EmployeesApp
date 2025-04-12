7017da7867dbf8d472f752058782d24bce8129c4
SashaGonchar <nekospoon228@gmail.com>
2025-04-12 15:37:54 +0300

    implemented search in the search bar in the searchPanel component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 {name: "Joe Biden", salary:10000, increase: true, rise: false, id: 2 },
                 {name: "Nick Pick", salary:7000, increase: false, rise: false, id: 3 },
                 {name: "Dark Rick", salary:3000, increase: false, rise: false, id: 4 },
-            ]
+            ],
+            term:''
         }
         this.maxId = 5;
     }
@@ -51,54 +52,49 @@ class App extends Component {
         });
     }
 
-    onToggleIncrease = (id) => {
+    onToggleProp = (id, prop) => {
 
         this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id){
-                    return {...item, increase: !item.increase}
+                    return {...item, [prop]: !item[prop]}
                 }
                 return item;
             })
         }))
-        //  this.setState(({data}) => {
-        //     const index = data.findIndex(elem => elem.id === id);
-        //      const old = data[index];
-        //      const newItem = {...old, increase: !old.increase};
-        //   const newArr = [...data.slice(0,index), newItem, ...data.slice(index + 1)];
-        //
-        //     return {
-        //        data: newArr
-        //   }
-        // })
-
     }
 
-    onToggleRise = (id) => {
-        this.setState(({data}) => ({
-            data: data.map(item => {
-                if (item.id === id){
-                    return {...item, rise: !item.rise}
-                }
-                return item;
-            })
-        }))
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter (item => {
+            return item.name.indexOf(term) > -1
+            }
+
+        )
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term: term})
     }
 
     render() {
+        const {data, term} = this.state
+        const visibleData = this.searchEmp(data, term);
         return (
             <div className={"app"}>
                 <AppInfo data={this.state.data}/>
 
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
-                    onToggleIncrease={this.onToggleIncrease}
-                    onToggleRise={this.onToggleRise}
+                    onToggleProp={this.onToggleProp}
                 />
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
